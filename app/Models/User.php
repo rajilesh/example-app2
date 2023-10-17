08caf8d52d8bc161ae96ec5d21f15a617a4eafd6
Rajilesh Panoli <rajileshpanoli123@gmail.com>
2023-10-17 20:48:24 +0530

    fix user sync issue

--- app/Models/User.php
+++ app/Models/User.php
@@ -25,6 +25,9 @@ class User extends Authenticatable implements Syncable
      * @var array<int, string>
      */
     protected $fillable = [
+        'name',
+        'email',
+        'password',
     ];
 
     /**
--- database/migrations/php artisan tinker.php
+++ database/migrations/php artisan tinker.php
@@ -4,10 +4,10 @@ php artisan migrate
 
 php artisan tinker
 $tenant1 = App\Models\Tenant::create([]);
-$tenant1->domains()->create(['domain' => 'fooaaaa.localhost']);
+$tenant1->domains()->create(['domain' => 'foo.localhost']);
 
 $tenant2 = App\Models\Tenant::create([]);
-$tenant2->domains()->create(['domain' => 'baraa.localhost']);
+$tenant2->domains()->create(['domain' => 'bar.localhost']);
 
 App\Models\Tenant::all()->runForEach(function () {
     App\Models\User::factory()->create();
